9212e0d5db7d4e2b915ab6cefda1562a27bcd110
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-17 12:22:00 +0500

    Add Program file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace GeometrGuru
+{
+  class Program
+  {
+     static void Main()
+     {
+        Console.WriteLine("Enter the base triangle");
+        Console.Write("b: ");
+        string baseTriangle = Console.ReadLine();
+
+        Console.WriteLine("Enter the height triangle");
+        Console.Write("h: ");
+        string heightTriangle = Console.ReadLine();
+
+        System.Console.WriteLine("Calculating Area...");
+
+        decimal b = decimal.Parse(baseTriangle);
+        decimal h = decimal.Parse(heightTriangle);
+        decimal S = (b*h)/2;
+
+        System.Console.WriteLine("Area: " + S);
+
+
+
+
+     }
+  }
+}
